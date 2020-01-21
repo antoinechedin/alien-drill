83942477b72eb862979c4a576f6917fa895c2cf7
p1516923 <rendy.latbi@etu.univ-lyon1.fr>
2020-01-21 18:33:50 +0100

    anim rail + destroy

--- Assets/Scripts/Minecart.cs
+++ Assets/Scripts/Minecart.cs
@@ -5,12 +5,33 @@ using UnityEngine;
 public class Minecart : MonoBehaviour
 {
     public float moveSpeed = 5;
+    public float fallSpeed = 0.4f;
     public Vector3 target;
+
+    public List<Plank> listPlank;
+    public Rail rail1;
+    public Rail rail2;
+
 
     private void Update()
     {
-        if (transform.position == target) Destroy(gameObject);
-        else TravelTo(target);
+        if (transform.position.y < 1.52f) transform.position = new Vector3(transform.position.x, 1.52f, transform.position.z);
+        if (transform.position.y > 1.52f) transform.position -= fallSpeed * Vector3.up;
+
+        if (transform.position == target)
+        {
+            foreach (Plank p in listPlank)
+            {
+                p.goUnderground = true;
+                Destroy(p.gameObject, 2f);
+            }
+            rail1.goUnderground = true;
+            rail2.goUnderground = true;
+            Destroy(rail1.gameObject, 2f);
+            Destroy(rail2.gameObject, 2f);
+            Destroy(gameObject);
+        }
+        else if (transform.position.y == 1.52f) TravelTo(target);
     }
 
     private void TravelTo(Vector3 targetPosition)
